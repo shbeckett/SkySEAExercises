e968e924eff2641b877ff673e5003dab9b2b849d
Sarah Beckett <sarah.beckett@sky.uk>
2022-09-09 16:33:20 +0100

    initial commit

---
+++ Dog.java
@@ -0,0 +1,8 @@
+public class Dog {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    public void bark() {
+        System.out.println("Woof!");
+    }
+}
